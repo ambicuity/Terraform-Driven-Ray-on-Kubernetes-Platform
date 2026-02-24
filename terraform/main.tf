0f29c4d86074724969db09b9570f0f8fdefab7d3
Ritesh Rana <44251619+ambicuity@users.noreply.github.com>
2026-02-24 13:16:59 -0600

    feat(infra): Mitigate latent legendary problems (DiskPressure, ENI, CoreDNS) (#25)

--- terraform/main.tf
+++ terraform/main.tf
@@ -293,6 +293,7 @@ resource "aws_eks_addon" "addons" {
   resolve_conflicts_on_create = try(each.value.resolve_conflicts_on_create, "OVERWRITE")
   resolve_conflicts_on_update = try(each.value.resolve_conflicts_on_update, "OVERWRITE")
   service_account_role_arn    = try(each.value.service_account_role_arn, null)
+  configuration_values        = try(each.value.configuration_values, null)
 
   depends_on = [
     aws_eks_node_group.cpu_workers,
